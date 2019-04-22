4b1e819edd023f8b9ac6ca82325eb6213afc889c
Gary Lai <garylai1990@gmail.com>
2019-04-22 15:30:54 -0400

    :bulb: (docs) add docs to code

--- genome_reader/genome.py
+++ genome_reader/genome.py
@@ -37,15 +37,47 @@ class Genome:
         return iter(self.dict)
 
     def keys(self):
+        """
+
+        Returns
+        -------
+        keys : dict_keys
+            get keys (rsid)
+        """
         return self.dict.keys()
 
     def values(self):
+        """
+
+        Returns
+        -------
+        values : dict_values
+            get values (snp)
+        """
         return self.dict.values()
 
     def items(self):
+        """
+
+        Returns
+        -------
+        items : dict_items
+            get both key and values (rsid, snp)
+        """
         return self.dict.items()
 
     def chromosome(self, key):
+        """
+
+        Parameters
+        ----------
+        key : str or int
+            find all SNPs whose chromosome is the value of the key
+        Returns
+        -------
+        SNPs : List
+            list of SNPs
+        """
         def search(snp, key):
             return snp if snp.chromosome == key else None
         return list(filter(lambda snp: search(snp, str(key)), self.dict.values()))
